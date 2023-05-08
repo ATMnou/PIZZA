0dac7885e18eae1c057fa339e00f85bcf33f67c3
ATMnou <herobrin708@gmail.com>
2023-05-09 00:47:20 +0900

    모달창

--- js/main.js
+++ js/main.js
@@ -182,3 +182,24 @@ spyEls.forEach(function (spyEl) {
  */
 const thisYear = document.querySelector('.this-year');
 thisYear.textContent = new Date().getFullYear();
+
+
+const modal = document.querySelector(".modal");
+const img = document.querySelector("#img");
+const modal_img = document.querySelector(".modal_content");
+const span = document.querySelector(".close");
+
+img.addEventListener('click', ()=>{
+  console.log("ye")
+  modalDisplay("block");
+  modal_img.src = img.src;
+});
+span.addEventListener('click', ()=>{
+  modalDisplay("none");
+});
+modal.addEventListener('click', ()=>{
+  modalDisplay("none");
+});
+function modalDisplay(text){
+  modal.style.display = text;
+}
